15502f5d7d1705454ad140864c8e4db9697415c5
Rommel Madriaga <2213724@slu.edu.ph>
2024-12-10 20:55:43 +0800

    modified the tests

--- test/test_services.py
+++ test/test_services.py
@@ -1,10 +1,10 @@
 import os
 import sys
-import unittest
 
 sys.path.insert(0, os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..')))
 
+import unittest
 from osa.models import Student, User
 from osa.services import OSASystem
 
